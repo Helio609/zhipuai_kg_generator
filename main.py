6f983bb506c007680853ab01dbf4bc5fd76e9f0b
Helio <helio609.dev@outlook.com>
2024-04-28 18:23:45 +0800

    support neo4j

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import logging
+import os
 
 from rich.logging import RichHandler
 
 from args import Parser
 from generator import Generator
+from neo4j_driver import Neo4jDriver
 from pdf import convert_pdf_to_text
 
 FORMAT = "[%(name)s] %(message)s"
@@ -17,6 +19,9 @@ logging.getLogger("httpcore.connection").setLevel(level="ERROR")
 logging.getLogger("urllib3.connectionpool").setLevel(level="ERROR")
 logging.getLogger("graphviz.saving").setLevel(level="ERROR")
 logging.getLogger("graphviz.backend.execute").setLevel(level="ERROR")
+logging.getLogger("neo4j").setLevel(level="ERROR")
+logging.getLogger("neo4j.io").setLevel(level="ERROR")
+logging.getLogger("neo4j.pool").setLevel(level="ERROR")
 
 
 def main():
@@ -29,8 +34,16 @@ def main():
         with open(args.input, "r", encoding="utf-8") as f:
             content = f.read()
 
+    driver: Neo4jDriver | None = None
+    if args.neo4j:
+        driver = Neo4jDriver(
+            os.getenv("NEO4J_URI"),
+            os.getenv("NEO4J_USERNAME"),
+            os.getenv("NEO4J_PASSWORD"),
+        )
+
     generator = Generator(model=args.model, main_language=args.language)
-    generator.generate(
+    knowledge_graph = generator.generate(
         content,
         args.output,
         args.window,
@@ -38,6 +51,10 @@ def main():
         split=args.split,
         update=args.update,
     )
+
+    if args.neo4j:
+        driver.insert_knowledge_graph(knowledge_graph)
+        driver.close()
 
 
 if __name__ == "__main__":
